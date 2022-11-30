3e69a2c398e6830d8fd08481eb34b1268f6e79c1
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2022-11-30 15:35:10 +0100

    Add query to show all ensembles next week as well as the remaining seats

--- queries.sql
+++ queries.sql
@@ -21,22 +21,25 @@ SUM(CASE WHEN l.lesson_type_id = 2 THEN 1 ELSE 0 END) AS group,
 SUM(CASE WHEN l.lesson_type_id = 3 THEN 1 ELSE 0 END) AS ensemble
 FROM lesson AS l 
 INNER JOIN booking AS b ON b.booking_id = l.booking_id 
-INNER JOIN lesson_type AS t ON t.lesson_type_id=l.lesson_type_id
 WHERE b.date BETWEEN '2022-01-01' AND '2022-12-31'
 GROUP BY month
 ORDER BY month;
 
--- List all ensembles next week sorted by genre and weekday
- 2022-12-19
- 2022-12-21
- 2022-12-23
-
-
-
-
-
-
-
+-- List all ensembles next week sorted by genre and weekday. Since there are only 24 ensembles this year it might return null.
+SELECT l.booking_id AS booking_id, b.date, EXTRACT(dow from b.date) AS day_of_week, t.type AS type, g.genre AS genre, 
+CASE
+    WHEN l.max_number_of_students - COUNT(*) FILTER (WHERE student_id IN (SELECT student_id FROM booked_students)) = 0 THEN 'fully booked'
+    WHEN l.max_number_of_students - COUNT(*) FILTER (WHERE student_id IN (SELECT student_id FROM booked_students)) > 2 THEN 'more seats left'
+    WHEN l.max_number_of_students - COUNT(*) FILTER (WHERE student_id IN (SELECT student_id FROM booked_students)) BETWEEN 1 AND 2 THEN '1-2 seats left'
+END AS seats
+FROM lesson AS l 
+INNER JOIN booking AS b ON b.booking_id = l.booking_id 
+INNER JOIN lesson_type AS t ON t.lesson_type_id = l.lesson_type_id
+INNER JOIN genre AS g ON g.genre_id = l.genre_id
+INNER JOIN booked_students AS bk ON bk.booking_id=l.booking_id
+WHERE b.date BETWEEN CURRENT_DATE AND (SELECT CURRENT_DATE + INTERVAL '7 DAY') --Remove row for all ensembles
+GROUP BY day_of_week, type, genre, l.booking_id, l.max_number_of_students, b.date
+ORDER BY day_of_week, genre;
 
 -- total lessons per month
 SELECT EXTRACT(MONTH from b.date) AS month, COUNT(*) AS lessons_given 
@@ -51,7 +54,6 @@ ORDER BY month;
 SELECT EXTRACT(MONTH from b.date) AS month, COUNT(l.lesson_type_id) AS individual
 FROM lesson AS l 
 INNER JOIN booking AS b ON b.booking_id = l.booking_id 
-INNER JOIN lesson_type AS t ON t.lesson_type_id=l.lesson_type_id
 WHERE b.date BETWEEN '2022-01-01' AND '2022-12-31' AND l.lesson_type_id = 1
 GROUP BY month
 ORDER BY month;
@@ -60,7 +62,6 @@ ORDER BY month;
 SELECT EXTRACT(MONTH from b.date) AS month, COUNT(l.lesson_type_id) AS group
 FROM lesson AS l 
 INNER JOIN booking AS b ON b.booking_id = l.booking_id 
-INNER JOIN lesson_type AS t ON t.lesson_type_id=l.lesson_type_id
 WHERE b.date BETWEEN '2022-01-01' AND '2022-12-31' AND l.lesson_type_id = 2
 GROUP BY month
 ORDER BY month;
@@ -69,7 +70,6 @@ ORDER BY month;
 SELECT EXTRACT(MONTH from b.date) AS month, COUNT(l.lesson_type_id) AS ensemble
 FROM lesson AS l 
 INNER JOIN booking AS b ON b.booking_id = l.booking_id 
-INNER JOIN lesson_type AS t ON t.lesson_type_id=l.lesson_type_id
 WHERE b.date BETWEEN '2022-01-01' AND '2022-12-31' AND l.lesson_type_id = 3
 GROUP BY month
 ORDER BY month;
