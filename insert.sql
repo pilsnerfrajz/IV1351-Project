d688196187931f6e717e60c3a823ac3548ae79de
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2022-12-01 10:17:28 +0100

    Add views

--- insert.sql
+++ insert.sql
@@ -1019,3 +1019,48 @@ AND price_skill_level_id=(SELECT price_skill_level_id FROM price_skill_level WHE
 (98,3,NULL,7,10,3,2,NULL),
 (99,3,NULL,7,10,3,5,NULL),
 (100,3,NULL,7,10,3,5,NULL);
+
+-- CREATE VIEW for siblings
+CREATE VIEW show_siblings AS
+SELECT 
+SUM(CASE WHEN student_id NOT IN (SELECT student_id FROM sibling) THEN 1 ELSE 0 END) AS no_sibling,
+(SELECT COUNT(*) FROM (SELECT student_id, COUNT(student_id) FROM sibling GROUP BY student_id HAVING COUNT(sibling_id) = 1) AS one) AS one_sibling,
+(SELECT COUNT(*) FROM (SELECT student_id, COUNT(student_id) FROM sibling GROUP BY student_id HAVING COUNT(sibling_id) = 2) AS two) AS two_siblings
+FROM student;
+
+-- CREATE VIEW for lessons
+CREATE VIEW lessons_per_month AS
+SELECT EXTRACT(YEAR from b.date) AS year, EXTRACT(MONTH from b.date) AS month,
+COUNT(*) AS lessons_given,
+SUM(CASE WHEN l.lesson_type_id = 1 THEN 1 ELSE 0 END) AS individual,
+SUM(CASE WHEN l.lesson_type_id = 2 THEN 1 ELSE 0 END) AS group,
+SUM(CASE WHEN l.lesson_type_id = 3 THEN 1 ELSE 0 END) AS ensemble
+FROM lesson AS l 
+INNER JOIN booking AS b ON b.booking_id = l.booking_id
+GROUP BY month, year
+ORDER BY month;
+
+-- CREATE VIEW for the ensembles the next week
+CREATE MATERIALIZED VIEW ensembles_next_week AS
+SELECT l.booking_id AS booking_id, b.date, EXTRACT(dow from b.date) AS day_of_week, t.type AS type, g.genre AS genre, 
+CASE
+    WHEN l.max_number_of_students - COUNT(*) FILTER (WHERE student_id IN (SELECT student_id FROM booked_students)) = 0 THEN 'fully booked'
+    WHEN l.max_number_of_students - COUNT(*) FILTER (WHERE student_id IN (SELECT student_id FROM booked_students)) > 2 THEN 'more seats left'
+    WHEN l.max_number_of_students - COUNT(*) FILTER (WHERE student_id IN (SELECT student_id FROM booked_students)) BETWEEN 1 AND 2 THEN '1-2 seats left'
+END AS seats
+FROM lesson AS l 
+INNER JOIN booking AS b ON b.booking_id = l.booking_id 
+INNER JOIN lesson_type AS t ON t.lesson_type_id = l.lesson_type_id
+INNER JOIN genre AS g ON g.genre_id = l.genre_id
+INNER JOIN booked_students AS bk ON bk.booking_id=l.booking_id
+WHERE (SELECT EXTRACT(WEEK FROM b.date)) = (SELECT EXTRACT(WEEK FROM NOW() + INTERVAL '1 WEEK'))
+GROUP BY day_of_week, type, genre, l.booking_id, l.max_number_of_students, b.date
+ORDER BY day_of_week, genre;
+
+-- CREATE VIEW for showing instructor lessons the current month
+CREATE VIEW given_lessons AS 
+SELECT i.first_name, i.last_name, i.person_number, COUNT(*) AS lessons_given FROM instructor AS i 
+INNER JOIN booking AS b ON i.instructor_id = b.instructor_id 
+WHERE (SELECT EXTRACT(MONTH FROM b.date)) = (SELECT EXTRACT(MONTH FROM NOW()))
+GROUP BY i.first_name, i.last_name, i.person_number
+ORDER BY COUNT(*) DESC;
